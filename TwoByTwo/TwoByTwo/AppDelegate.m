67f10642937556d0b795d3f36f9684541d7ee954
John Tubert <jtubert@hotmail.com>
2013-09-15 23:03:18 -0400

    adding parse library

--- TwoByTwo/TwoByTwo/AppDelegate.m
+++ TwoByTwo/TwoByTwo/AppDelegate.m
@@ -7,12 +7,18 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"gEBi2TwNiWHmFNvQ1p6sl5iqcVnZkPZeVmNKDC96"
+                  clientKey:@"uVtNeJqF9FuxR8EEmFyqLlPNA8j4jJpFOJarSOdj"];
+    
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
     return YES;
 }
 							
