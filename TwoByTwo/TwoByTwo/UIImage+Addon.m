47e8f3a152e3e81ed8e42892948b4e2cba8fb7d2
John Tubert <jtubert@hotmail.com>
2014-03-10 09:03:36 -0400

    change font on watermark

--- TwoByTwo/TwoByTwo/UIImage+Addon.m
+++ TwoByTwo/TwoByTwo/UIImage+Addon.m
@@ -58,20 +58,23 @@
 
 - (UIImage *)imageWithWatermark:(NSString *)text
 {
-    NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:11], NSForegroundColorAttributeName:[UIColor whiteColor]};
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont appLightFontOfSize:6], NSForegroundColorAttributeName:[UIColor whiteColor]};
     
         
     CGRect rect = [text boundingRectWithSize:CGSizeMake(self.size.width, MAXFLOAT)
                                      options:NSStringDrawingUsesLineFragmentOrigin
                                   attributes:attributes
                                      context:nil];
-    CGRect textRect = CGRectMake(self.size.width - rect.size.width, self.size.height - rect.size.height, rect.size.width, rect.size.height);
+    CGRect textRect = CGRectMake(self.size.width - rect.size.width-5, self.size.height - rect.size.height-5, rect.size.width, rect.size.height);
+    
+    //CGRectMake(CGFloat x, CGFloat y, CGFloat width, CGFloat height)
+    CGRect backgRect = CGRectMake(self.size.width - rect.size.width-10, self.size.height - rect.size.height-8, rect.size.width+10, rect.size.height+8);
     
     UIGraphicsBeginImageContextWithOptions(self.size, YES, 0.0);
     [self drawInRect:CGRectMake(0, 0, self.size.width, self.size.height)];
     
     [[UIColor blackColor] set];
-    CGContextFillRect( UIGraphicsGetCurrentContext(), textRect);
+    CGContextFillRect( UIGraphicsGetCurrentContext(), backgRect);
     
     
     [text drawInRect:textRect withAttributes:attributes];
